fa7e28b8a2436147710ca08f7c40e45246624783
Oliver Ren <oliveren@mit.edu>
2016-01-20 13:07:12 -0500

    test?

--- javascripts/orders.js
+++ javascripts/orders.js
@@ -2,16 +2,26 @@
  * 
  */
 Parse.initialize("pEZtAFuzsjCmkxrPrdpwD0zWvXSmB0GWSWPat9rF", "odmOFVGIjpFNje7yqEUMS4DzAJ5eU2T8soL0kScl");
+
 var order = new Parse.Object("Order");
 var query = new Parse.Query(order);
+var text = "";
+query.notEqualTo("name", "vishy");
 query.find({
   success: function(results) {
     for (i = 0; i < results.length; i++) {
-    	document.querySelector('.test').innerHTML += results[i].id + "\r\n";
+    	document.getElementById("test").innerHTML += results[i].get("name") + ": " + results[i].id +  "\r\n";
+    	text += "hello";
     }
+    console.log(document.getElementById("test").innerHTML);
+    console.log(text);
   },
 
   error: function(error) {
 	  document.write("error");
   }
 });
+console.log(document.getElementById("test").innerHTML);
+document.getElementById("test").innerHTML = text;
+console.log(document.getElementById("test").innerHTML);
+console.log(text);
